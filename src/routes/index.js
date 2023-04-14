2b27a7943644db7199ba9bd758c8f633260ab41c
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2023-04-14 10:20:51 -0300

    now router ok?

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 
 // pages
-import Home from "../components/templates/home";
 import Initial from "../pages";
 
 const router = createBrowserRouter([
